b79a7f10fa4eb65bba4843d36b58d71e49642528
davidcreager <david.h.creager@btinternet.com>
2020-11-07 16:30:50 +0000

    Adding Test for first itweration

---
+++ testxiaomi.js
@@ -0,0 +1,19 @@
+'use strict';
+const logs = require("./logs");
+const dev =require("./BTAgent");
+var log = new logs.Log("tester",false,true);
+
+//	AgentOptions = {type: "adverts|connected", log: null, 
+//					address: "uuid",
+//					bindKey: "",
+//					handler:null, 
+//					options:{}}
+let testDevice = new dev({"address": "a4:c1:38:f7:92:27",
+							"bindKey": "eed67d3cec84bee92fd3a304978c76a0",
+							"log": log})
+
+
+//console.log("enum warn = " + log.LogLevel.WARN);
+//console.log("enum info = " + log.LogLevel.INFO);
+//console.log("debugEnabled = " + log.debugEnabled);
+log.warn("hello");
